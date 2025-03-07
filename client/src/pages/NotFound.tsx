8afbf8f36b3679a1be33ec4e8816aa8685ed88f9
Pablo Valdes <pablovaldes0925@gmail.com>
2025-03-07 00:33:35 -0500

    404 page

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,20 +1,26 @@
+import { Link } from 'react-router-dom'
 
 const NotFound = () => {
  
   return (
-    <div id="not-found" className="not-found-bg flex flex-col justify-center items-center h-screen">
-      <div>
-        <div className="top">
-          <h1>404</h1>
+    <div id="not-found" className="not-found-bg flex flex-col justify-center items-center min-h-screen h-min">
+      <div className="h-screen flex flex-col justify-center items-center ">
+
+        <div className="bg-white p-4 rounded-4xl dark-shadow w-[400px] h-[200px] flex justify-center items-center">
+          <h1 className="bungee-regular text-black text-8xl">?!?</h1>
         </div>
-        <div className="bottom">
-          <div>
-            <h1></h1>
+
+        <div className="bg-white p-4 rounded-4xl dark-shadow w-[800px] h-[500px] flex flex-col mt-4">
+          <div className="h-[240px] w-full text-center flex justify-center items-end">
+            <h1 className="cabin-sketch-regular text-black text-4xl">The page you are looking<br/>for does not exist :&#40;</h1>
           </div>
-          <div>
-            <h1>not found</h1>
+          <div className="h-[260px] w-full flex justify-center items-center">
+            <Link to="/">
+              <h1 className="cabin-sketch-regular text-black text-3xl">Go Home &gt;</h1>
+            </Link>
           </div>
         </div>
+
       </div>
     </div>
   )
